2c9f8d351f227d49c5473bd251943f9fa1d6818c
gelo <angelotorres200425@gmail.com>
2025-05-05 21:47:06 +0800

    enable editing of email and trainer experience in profile

--- profilee/serializers.py
+++ profilee/serializers.py
@@ -3,12 +3,12 @@ from account.models import CustomUser
 
 class ProfileSerializer(serializers.ModelSerializer):
     membership_status = serializers.SerializerMethodField()
-    experience = serializers.SerializerMethodField()
+    experience = serializers.CharField(required=False, allow_blank=True)
 
     class Meta:
         model = CustomUser
         fields = [
-            'full_name', 'contact_number', 'messenger_account', 'complete_address',
+            'email', 'full_name', 'contact_number', 'messenger_account', 'complete_address',
             'nationality', 'birthdate', 'gender',
             'height', 'weight', 'age',
             'type_of_membership', 'membership_status', 'experience'
@@ -17,10 +17,32 @@ class ProfileSerializer(serializers.ModelSerializer):
     def get_membership_status(self, obj):
         return "Active" if obj.is_membership_active else "Inactive"
 
-    def get_experience(self, obj):
-        if obj.is_trainer and hasattr(obj, 'trainer_profile'):
-            return obj.trainer_profile.experience
-        return None
+    def to_representation(self, instance):
+        # Overrides get_experience logic during serialization
+        data = super().to_representation(instance)
+        if instance.is_trainer and hasattr(instance, 'trainer_profile'):
+            data['experience'] = instance.trainer_profile.experience
+        else:
+            data['experience'] = None
+        return data
+
+    #  (PATCH) for both email and trainer_profile.experience
+    def update(self, instance, validated_data):
+        # Update fields from CustomUser
+        experience = validated_data.pop('experience', None)
+        email = validated_data.get('email')
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        # Handle experience separately for trainer
+        if instance.is_trainer and hasattr(instance, 'trainer_profile') and experience is not None:
+            instance.trainer_profile.experience = experience
+            instance.trainer_profile.save()
+
+        return instance
+
 
 class ChangePasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
