c6b6f40fc4c5353b6170b3fc5084ede02579c3d0
gelo <angelotorres200425@gmail.com>
2025-05-30 10:15:41 +0800

    added full_name in registration

--- account/serializers.py
+++ account/serializers.py
@@ -8,13 +8,14 @@ User = get_user_model()
 
 # --- REGISTRATION ---
 class RegistrationSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(required=True)
     password = serializers.CharField(write_only=True)
     confirm_password = serializers.CharField(write_only=True)
     is_trainer = serializers.BooleanField(default=False)
 
     class Meta:
         model = User
-        fields = ['email', 'password', 'confirm_password', 'is_trainer']
+        fields = ['email', 'full_name', 'password', 'confirm_password', 'is_trainer']
         extra_kwargs = {'password': {'write_only': True}}
 
     def validate(self, data):
@@ -24,14 +25,15 @@ class RegistrationSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({"email": "A user with this email already exists."})
         return data
 
-    # serializers.py
     def create(self, validated_data):
         is_trainer = validated_data.pop('is_trainer', False)
+        full_name = validated_data.pop('full_name')
         user = User.objects.create_user(
             email=validated_data['email'],
             password=validated_data['password'],
+            full_name=full_name,
             is_trainer=is_trainer,
-            is_active=True if is_trainer else False, # Trainers active by default
+            is_active=True if is_trainer else False,
             membership_start_date=None,
             membership_end_date=None,
         )
